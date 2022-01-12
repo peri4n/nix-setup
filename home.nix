4891d1c42c45bacba0b61e0225d1a8837607453d
Fabian Bull <fabian@fbull.de>
2022-01-12 21:44:26 +0100

    Add XDG default applications

--- home.nix
+++ home.nix
@@ -93,7 +93,13 @@
     mimeApps = {
       enable = true;
       defaultApplications = {
-          "application/pdf" = [ "zathura.desktop" ];
+          "application/pdf" = [ "org.pwmt.zathura.desktop" ];
+          "x-scheme-handler/jetbrains" = [ "jetbrains-toolbox.desktop" ];
+          "text/html" = [ "vivaldi-stable.desktop" ];
+          "x-scheme-handler/http" = [ "vivaldi-stable.desktop" ];
+          "x-scheme-handler/https" = [ "vivaldi-stable.desktop" ];
+          "x-scheme-handler/about" = [ "vivaldi-stable.desktop" ];
+          "x-scheme-handler/unknown" = [ "vivaldi-stable.desktop" ];
         };
     };
   };
