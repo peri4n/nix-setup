ab75d72e939c09f768cf0085ea786e0641098934
Fabian Bull <fabian@fbull.de>
2022-02-06 21:53:17 +0100

    Improve next match behaviour

--- programs/nvim/lua/mappings.lua
+++ programs/nvim/lua/mappings.lua
@@ -1,6 +1,9 @@
 vim.api.nvim_set_keymap('n', '<leader>ev', ':vsplit $MYVIMRC<cr>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>sv', ':source $MYVIMRC<cr>', {noremap = true, silent = true})
 
+vim.api.nvim_set_keymap('n', 'n', 'nzz', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'N', 'Nzz', {noremap = true, silent = true})
+
 vim.api.nvim_set_keymap('n', '<down>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<up>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<left>', '<Nop>', {noremap = true, silent = true})
