7454afc34906c8f2a6f2be51d9ecdb76543bac79
Fabian Bull <fabian@fbull.de>
2022-02-08 18:59:00 +0100

    Add convenient mappings for visual mode

--- programs/nvim/lua/mappings.lua
+++ programs/nvim/lua/mappings.lua
@@ -4,6 +4,13 @@ vim.api.nvim_set_keymap('n', '<leader>sv', ':source $MYVIMRC<cr>', {noremap = tr
 vim.api.nvim_set_keymap('n', 'n', 'nzz', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', 'N', 'Nzz', {noremap = true, silent = true})
 
+-- make . work with visually selected lines
+vim.api.nvim_set_keymap('v', '.', ':normal.<cr>', {noremap = true, silent = true})
+
+-- Move visual selection
+vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', 'K', ":m '<-2<CR>gv=gv", {noremap = true, silent = true})
+
 vim.api.nvim_set_keymap('n', '<down>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<up>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<left>', '<Nop>', {noremap = true, silent = true})
