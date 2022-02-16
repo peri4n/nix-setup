710fdd318f786b02c9380480fdaf792412976841
Fabian Bull <fabian@fbull.de>
2022-02-16 20:32:33 +0100

    Start working on the broot dracula theme

---
+++ dracula.nix
@@ -0,0 +1,32 @@
+{
+  dracula = {
+    hex = {
+      background = "#282a36";
+      currentLine = "#44475a";
+      selection = "#44475a";
+      foreground = "#f8f8f2";
+      comment = "#6272a4";
+      cyan = "#8be9fd";
+      green = "#50fa7b";
+      orange = "#ffb86c";
+      pink = "#ff79c6";
+      purple = "#bd93f9";
+      red = "#ff5555";
+      yellow = "#f1fa8c";
+    };
+    rgb = {
+      background = "40 42 54";
+      currentLine = "40 42 54";
+      selection = "68 71 90";
+      foreground = "248 248 242";
+      comment = "98 114 164";
+      cyan = "139 233 253";
+      green = "80 250 123";
+      orange = "255 184 108";
+      pink = "255 121 198";
+      purple = "189 147 249";
+      red = "255 85 85";
+      yellow = "241 250 140";
+    };
+  };
+}
--- programs/broot.nix
+++ programs/broot.nix
@@ -1,8 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let 
+  colors = import ../dracula.nix;
+  toRgb = with lib.strings; str: "rgb(" + concatStringsSep ", " (splitString " " str) + ")";
+in
 {
   programs.broot = {
     enable = true;
     enableZshIntegration = true;
+    skin = {
+      default = toRgb colors.dracula.rgb.foreground + " " + toRgb colors.dracula.rgb.background;
+      directory = toRgb colors.dracula.rgb.purple + " " + toRgb colors.dracula.rgb.background;
+      input = "none none";
+      status_normal = "none none";
+      status_italic = toRgb colors.dracula.rgb.yellow + " none";
+      status_bold = toRgb colors.dracula.rgb.yellow + " none";
+      status_job = toRgb colors.dracula.rgb.yellow + " none";
+      status_code = toRgb colors.dracula.rgb.yellow + " none";
+      status_ellipsis = toRgb colors.dracula.rgb.yellow + " none";
+      flag_value = toRgb colors.dracula.rgb.yellow + " none";
+      flag_label = "none none";
+    };
   };
 }
