d406636ceadd06d5c4e4ec5d75f40602bb1ee472
Fabian Bull <fabian@fbull.de>
2022-03-26 19:27:40 +0100

    Add metals LSP for Scala

--- programs/nvim/lua/lsp.lua
+++ programs/nvim/lua/lsp.lua
@@ -46,13 +46,16 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp .protocol .make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = {"hls", "rnix", "rust_analyzer", "tsserver", "pylsp", "jdtls" }
+local servers = {"hls", "rnix", "rust_analyzer", "tsserver", "pylsp", "jdtls", "metals" }
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {on_attach = on_attach, capabilities = capabilities}
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities
+  }
 end
 
 local runtime_path = vim.split(package.path, ';')
