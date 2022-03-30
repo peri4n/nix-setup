13c8c318ea97b6e112786b1af284af2dfe48653e
Fabian Bull <fabian@fbull.de>
2022-03-30 10:31:45 +0200

    Display relative path in NeoVim statusline

--- programs/nvim/lua/lualine.lua
+++ programs/nvim/lua/lualine.lua
@@ -6,7 +6,12 @@ require('lualine').setup({
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename', 'lsp_progress' },
+    lualine_c = {
+      {
+        'filename',
+        path = 1 -- relatife paths
+      },
+     'lsp_progress' },
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
