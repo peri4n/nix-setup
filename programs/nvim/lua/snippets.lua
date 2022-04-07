1775bf18c2aac567296102595669695a0bf33ebf
Fabian Bull <fabian@fbull.de>
2022-04-07 21:00:03 +0200

    Add lua snippets for markdown and all files

--- programs/nvim/lua/snippets.lua
+++ programs/nvim/lua/snippets.lua
@@ -1,9 +1,59 @@
 local ls = require('luasnip')
 
+-- some shorthands...
+local s = ls.snippet
+local node = ls.snippet_node
+local text = ls.text_node
+local insert = ls.insert_node
+local func = ls.function_node
+local choice = ls.choice_node
+local dynamicn = ls.dynamic_node
+
+local date = function()
+  return os.date('%Y-%m-%d')
+ end
+
 ls.config.set_config({
     history = true,
     updateevents = "TextChanged,TextChangedI",
-    enable_autosnippets = true
+    enable_autosnippets = true,
+    store_selection_keys = '<c-s>'
 })
 
+ls.snippets = {
+  all = {
+    -- My signature
+    s({
+      trig = "signature",
+      name = "Signature",
+      dscr = "My name"
+    }, text("Fabian Bull")),
+
+    -- The current date
+    s({
+      trig = "date",
+      name = "Date",
+      dscr = "Date in the form of YYYY-MM-DD"
+    }, func(date)),
+  },
+  markdown = {
+    s({
+        trig = "meta",
+        name = "Metadata",
+        dscr = "Yaml metadata format for markdown"
+    }, { text({"---",
+        "title: "}), insert(1, "note_title"), text({"",
+        "author: "}), insert(2, "author"), text({"",
+        "date: "}), func(date, {}), text({"",
+        "categories: ["}), insert(3, ""), text({"]",
+        "lastmod: "}), func(date, {}), text({"",
+        "tags: ["}), insert(4), text({"]",
+        "---", ""}),
+        insert(0)
+      })
+  }
+}
+
 require("luasnip.loaders.from_vscode").load()
+
+vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<cr>")
