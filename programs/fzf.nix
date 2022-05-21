0d85e72296151c359b42b57ef104e423516c3063
Fabian Bull <fabian@fbull.de>
2022-05-21 15:09:07 +0200

    Use global Dracula theme in FZF

--- programs/fzf.nix
+++ programs/fzf.nix
@@ -1,14 +1,16 @@
 { config, pkgs, lib, ... }:
 
-{
+let 
+  colors = import ../dracula.nix;
+in {
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    defaultOptions = [ 
-      "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9"
-      "--color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9"
-      "--color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6"
-      "--color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4" ];
+    defaultOptions = with colors.dracula.hex; [ 
+      "--color=fg:${foreground},bg:${background},hl:${purple}"
+      "--color=fg+:${foreground},bg+:${selection},hl+:${purple}"
+      "--color=info:${orange},prompt:${green},pointer:${pink}"
+      "--color=marker:${pink},spinner:${orange},header:${comment}" ];
     historyWidgetOptions = [ "--preview 'bat --style=numbers --color=always --line-range :500 {}'" ];
   };
 }
