acab6a4624c1d819c239f6e7125398a5be31cc02
Fabian Bull <fabian@fbull.de>
2022-06-07 19:47:50 +0200

    Add treesitter info to lualine

--- programs/nvim/lua/lualine.lua
+++ programs/nvim/lua/lualine.lua
@@ -1,19 +1,26 @@
+local colors = require('dracula').colors()
+
 require('lualine').setup({
   options = {
     theme = 'dracula-nvim',
     globalstatus = true
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = {
       {
         'filename',
         path = 1 -- relative paths
       },
-   },
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+      '%=',
+      {
+        'require("nvim-treesitter").statusline()',
+        color = { fg = colors.orange }
+      }
+    },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
 })
