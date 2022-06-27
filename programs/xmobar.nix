09060e08510482dcef1aebd3c56d1f534b2ea12b
Fabian Bull <spam+github@fbull.de>
2022-06-27 14:37:48 +0200

    Continue to play with XMobar icons

--- programs/xmobar.nix
+++ programs/xmobar.nix
@@ -7,7 +7,7 @@ in
   programs.xmobar = with colors.dracula.hex; {
     enable = true;
     extraConfig = ''
-      Config { font = "xft:FiraCode Nerd Font:size=10:antialias=true:hinting=true"
+      Config { font = "xft:FiraCode Nerd Font:size=11:antialias=true:hinting=true"
              , additionalFonts = []
              , borderColor = "black"
              , border = TopB
@@ -25,9 +25,8 @@ in
              , allDesktops = True
              , overrideRedirect = True
              , alignSep = "}{"
-             , template = "%StdinReader% }{ %dynnetwork% | %memory% %battery% | %date%"
+             , template = "%StdinReader% }{ %dynnetwork% | %memory% | %battery% | %date%"
              , commands = [
-                          -- network activity monitor (dynamic interface resolution)
                             Run DynNetwork     [ "--template" , "<fc=${purple}><dev></fc>: <tx> kB/s <rx> kB/s"
                                                , "--Low"      , "1000000"       -- units: B/s
                                                , "--High"     , "5000000"       -- units: B/s
@@ -35,17 +34,16 @@ in
                                                , "--normal"   , "orange"
                                                , "--high"     , "#ff8080"
                                                ] 10
-                          , Run Memory ["-t","<fc=${purple}>\xf85a</fc> <usedratio>%"] 10
+                          , Run Memory ["-t","<usedratio>% <fc=${purple}> </fc>"] 10
                           , Run BatteryP ["BAT0"] [
                               "-t", "<fc=${purple}><acstatus></fc> <left>% - <timeleft>",
                               "--",
-                              --"-c", "charge_full",
                               "-O", "\xfba3",
                               "-o", "\xf578",
                               "-h", "green",
                               "-l", "red"
                               ] 10
-                          , Run Date "<fc=${orange}>%a %b %d %H</fc>:<fc=${orange}>%M</fc>" "date" 60
+                          , Run Date "<fc=${orange}> %a %b %d %H</fc>:<fc=${orange}>%M</fc>" "date" 60
                           , Run StdinReader
                           ]
              }
