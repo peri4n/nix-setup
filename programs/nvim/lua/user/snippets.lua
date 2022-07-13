7230fd24b2a6a17bd98f7f88b50b8ee32cd5e6a5
Fabian Bull <spam+github@fbull.de>
2022-07-13 10:00:47 +0200

    Improve startup time

--- programs/nvim/lua/user/snippets.lua
+++ programs/nvim/lua/user/snippets.lua
@@ -51,6 +51,6 @@ ls.snippets = {
   }
 }
 
-require("luasnip.loaders.from_vscode").load()
+require("luasnip.loaders.from_vscode").lazy_load()
 
 vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<cr>")
