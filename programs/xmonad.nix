bfa64d7232e6cfd66b2b62cce8fd811199037daa
Fabian Bull <spam+github@fbull.de>
2022-07-13 09:59:32 +0200

    Update XMonad to 1.0.7

--- programs/xmonad.nix
+++ programs/xmonad.nix
@@ -3,7 +3,7 @@
 let src = pkgs.fetchFromGitHub {
   owner = "peri4n";
   repo = "wm";
-  rev = "9fbd357f06805ac3dd3fabc91e953e5a3e283e2e";
+  rev = "fc6b1b276c6126a5e80fa0fb683c4301256fb4ce";
   sha256 = "hknBc0nL/IUcCgimHGi3CotmQvWC6f/gSgQepi1ratM=";
 };
 in
