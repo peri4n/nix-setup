f0081dfab0a6a7702a12eccc0ef284032fc17eda
Fabian Bull <spam+github@fbull.de>
2022-07-22 19:57:08 +0200

    Enable experiment Nix features

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,10 @@
       ./nvidia.nix
     ];
 
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
