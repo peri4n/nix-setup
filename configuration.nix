922c1a2d111f52db991fc43fd10d7f64fe40f12e
Fabian Bull <spam+github@fbull.de>
2022-07-30 17:25:07 +0200

    Pretify display manager

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,30 @@
     # Enable the X11 windowing system.
     enable = true;
 
+    displayManager = {
+      lightdm = {
+        enable = true;
+        background = pkgs.nixos-artwork.wallpapers.dracula.gnomeFilePath;
+
+        extraSeatDefaults = ''
+          greeter-show-manual-login=true
+          greeter-hide-users=true
+        '';
+
+        greeters.gtk = {
+          extraConfig = ''
+            default-user-image = ${pkgs.nixos-icons}/share/icons/hicolor/64x64/apps/nix-snowflake.png
+            position = 50%,center -300,end
+          '';
+
+          theme = {
+            name = "Dracula";
+            package = pkgs.dracula-theme;
+          };
+        };
+      };
+    };
+
     # Enable the GNOME Desktop Environment.
     displayManager = {
       defaultSession = "none+xmonad";
