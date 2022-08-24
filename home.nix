2185bd341d06b5658858abbbd4640ab95d194ed9
Fabian Bull <spam+github@fbull.de>
2022-08-24 20:09:02 +0200

    Add Dracula bemenu theme

--- home.nix
+++ home.nix
@@ -169,7 +169,11 @@
       # Quickly switch vi modes
       KEYTIMEOUT = 1;
 
-      GDK_SCALE = 1;
+      BEMENU_OPTS = ''
+      --tb '#6272a4' --tf '#f8f8f2' --fb '#282a36' --ff '#f8f8f2' --nb '#282a36' 
+      --nf '#6272a4' --hb '#44475a' --hf '#50fa7b' --sb '#44475a' --sf '#50fa7b' 
+      --scb '#282a36' --scf '#ff79c6'
+           '';
 
   };
 
