08c4b4b7258296e2b5257a5e226b457140c1c61f
Fabian Bull <spam+github@fbull.de>
2022-08-24 20:08:32 +0200

    Only fold markdown files at the start

--- programs/nvim/lua/user/markdown.lua
+++ programs/nvim/lua/user/markdown.lua
@@ -4,6 +4,6 @@ vim.g['vim_markdown_math'] = 1
 vim.g['vim_markdown_no_extensions_in_markdown'] = 0 -- Do require .md extensions for Markdown links
 
 vim.cmd([[
-autocmd BufRead,BufNewFile   *.md set foldlevel=2
+autocmd BufRead,BufNewFile   *.md set foldlevelstart=2
 ]])
 
