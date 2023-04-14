196b379777ebf7172f11d661efe51cbbc9746e79
Fabian Bull <spam+github@fbull.de>
2023-04-14 11:23:10 +0200

    Improve readability of comments

--- programs/nvim/lua/user/dracula.lua
+++ programs/nvim/lua/user/dracula.lua
@@ -1,5 +1,10 @@
 local dracula = require("dracula")
+local colors = dracula.colors()
+
 dracula.setup({
   show_end_of_buffer = false,
-  italic_comment = true
+  italic_comment = true,
+  overrides = {
+    Comment = { fg = colors.bright_cyan, italic = true },
+  }
 })
