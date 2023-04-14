cd6d70c8331ce2434468107911c1f575059be1b5
Fabian Bull <spam+github@fbull.de>
2023-04-14 11:22:39 +0200

    Remove easy window switching
    - Ctrl mappings are too valuable for this

--- programs/nvim/lua/user/mappings.lua
+++ programs/nvim/lua/user/mappings.lua
@@ -19,12 +19,6 @@ vim.keymap.set('n', '<up>', '<Nop>')
 vim.keymap.set('n', '<left>', '<Nop>')
 vim.keymap.set('n', '<right>', '<Nop>')
 
--- Easy window switching
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-
 -- Easy tab switching
 vim.keymap.set('n', '<S-h>', 'gT')
 vim.keymap.set('n', '<S-l>', 'gt')
