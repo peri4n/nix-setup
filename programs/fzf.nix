d7abc47e939393fbaa57ef6bc97bc1f753c1addd
Fabian Bull <spam+github@fbull.de>
2023-07-10 07:51:23 +0200

    Improve FZF setup in nix

--- programs/fzf.nix
+++ programs/fzf.nix
@@ -1,16 +1,31 @@
 { config, pkgs, lib, ... }:
 
-let 
+let
   colors = import ../themes/dracula.nix;
-in {
+in
+{
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    defaultOptions = with colors.dracula.hex; [ 
-      "--color=fg:${foreground},bg:${background},hl:${purple}"
-      "--color=fg+:${foreground},bg+:${selection},hl+:${purple}"
-      "--color=info:${orange},prompt:${green},pointer:${pink}"
-      "--color=marker:${pink},spinner:${orange},header:${comment}" ];
-    historyWidgetOptions = [ "--preview 'bat --style=numbers --color=always --line-range :500 {}'" ];
+    colors = with colors.dracula.hex; {
+      fg = foreground;
+      "fg+" = foreground;
+      bg = background;
+      "bg+" = selection;
+      hl = purple;
+      "hl+" = purple;
+      info = orange;
+      marker = pink;
+      prompt = green;
+      spinner = orange;
+      pointer = pink;
+      header = comment;
+    };
+    defaultCommand = "${pkgs.fd}/bin/fd --type f";
+    defaultOptions = [
+      "--height 40%"
+    ];
+    fileWidgetCommand = "${pkgs.fd}/bin/fd --type f";
+    fileWidgetOptions = [ "--preview 'bat --style=numbers --color=always --line-range :500 {}'" ];
   };
 }
