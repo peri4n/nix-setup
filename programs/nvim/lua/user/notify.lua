6d74ef61161055f88a68636c4fbbc197b1c12632
Fabian Bull <spam+github@fbull.de>
2023-12-14 19:24:18 +0100

    Silence NeoVim LSP notifications

--- programs/nvim/lua/user/notify.lua
+++ programs/nvim/lua/user/notify.lua
@@ -83,13 +83,3 @@ vim.lsp.handlers["$/progress"] = function(_, result, ctx)
    notif_data.spinner = nil
  end
 end
-
-local severity = {
-  "error",
-  "warn",
-  "info",
-  "info", -- map both hint and info to info?
-}
-vim.lsp.handlers["window/showMessage"] = function(err, method, params, client_id)
-             vim.notify(method.message, severity[params.type])
-end
