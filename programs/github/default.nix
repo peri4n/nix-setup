61cce1bd19eac1f30a0c33070fa1056e678dddad
Fabian Bull <spam+github@fbull.de>
2024-02-09 19:52:29 +0100

    Remove Github Dashboard
    - Fix to enable copilot

--- programs/github/default.nix
+++ programs/github/default.nix
@@ -3,7 +3,6 @@
   programs.gh = {
     enable = true;
     gitCredentialHelper.enable = true;
-    extensions = [ pkgs.gh-dash ];
     settings = {
       git_protocol = "ssh";
       editor = "nvim";
