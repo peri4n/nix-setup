11aa3cf1b23c69288ef168d53bcb85d63bc0dcac
Fabian Bull <spam+github@fbull.de>
2024-10-09 17:20:28 +0200

    Improve Nvim flash bindings

--- programs/nvim/lua/user/flash.lua
+++ programs/nvim/lua/user/flash.lua
@@ -1,7 +1,7 @@
-require('flash').setup({
-  modes = {
-    search = {
-      enabled = true
-    }
-  }
-})
+local flash = require('flash')
+flash.setup()
+
+vim.keymap.set({'n', 'x', 'o'}, 's', function() flash.jump() end, {})
+vim.keymap.set({'n', 'x', 'o'}, 'S', function() flash.treesitter() end, {})
+vim.keymap.set({'o'}, 'r', function() flash.remote() end, {})
+vim.keymap.set({'x', 'o'}, 'R', function() flash.treesitter_search() end, {})
