74f1fe1d410b55c9f08dda91c22cdd8789e94d38
Fabian Bull <spam+github@fbull.de>
2024-11-07 21:05:24 +0100

    Enable bluetooth

--- hardware/senec/hardware-configuration.nix
+++ hardware/senec/hardware-configuration.nix
@@ -36,4 +36,5 @@
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.bluetooth.enable = true;
 }
